29e47080515e42f44a410cd41b8baaa91586d0bd
NihalSh <nihal.sh.1995@gmail.com>
2017-02-12 20:24:10 +0530

    personal details implemented

--- handlers/details.js
+++ handlers/details.js
@@ -1,3 +1,26 @@
-module.exports = function(req,res){
-	res.send('#TODO');
+"use strict";
+let request = require('request');
+
+let options = require('./requests/course-confirmation-report.js');
+let parser = require('./parsers/details.js');
+
+module.exports = function(req, res){
+	if(req.session.jar){
+		req.log.info("authorized user");
+		options.headers['Cookie'] = req.session.jar;
+		request(options, function(error, response, body){
+				if(!error){
+					req.log.info("details request successful");
+					res.send(parser(body));
+				}else{
+					req.log.info("details request failed");
+					req.log.error(error);
+					res.send("Error fetching details!");
+				}
+			}
+		);
+	}else{
+		req.log.info("unauthorized user");
+		res.sendStatus(404);
+	}
 };
---
+++ handlers/parsers/details.js
@@ -0,0 +1,25 @@
+var cheerio = require('cheerio');
+
+module.exports = function extractData(body) {
+	"use strict"
+	let $ = cheerio.load(body);
+
+	let key = "";
+	let regex = /([A-Za-z]+[\s]*[A-Za-z]+):/;
+	let details = {};
+	$('table[align="left"]').find('tr').each( function (index, element) {
+			$(element).find('td').each( function (index, element) {
+					if ($(element).find('strong').length) {
+						if (this.key.search(this.re) != -1) {
+							this.details[this.key.match(this.re)[1]] = $(element).text();
+						}
+						this.key = "";
+					} else {
+						this.key += $(element).text();
+					}
+				}.bind({'key': key, 're': regex, 'details': details})
+			);
+		}.bind({'key': key, 're': regex, 'details': details})
+	);
+	return details;
+};
