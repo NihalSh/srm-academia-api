a023c091175501ae18ffa8998a113d1bd1688d46
NihalSh <nihal.sh.1995@gmail.com>
2017-02-13 09:19:42 +0530

    header extraction

--- handlers/parsers/schedule.js
+++ handlers/parsers/schedule.js
@@ -4,22 +4,34 @@ module.exports = function parser(body) {
 	"use strict"
 	let $ = cheerio.load(body);
 	
-	let schedule = $('table[align="center"]').html();
-	console.log(schedule);
+	//header extraction
+	let schedule = [];
+	let month = [];
+	$('table[align="center"]').find('th').each(function(index, element){
+			if((index+1)%5 !== 0){
+				let content = {};
+				content[$(element).text()] = [];
+				month.push(content);
+			}else{
+				schedule.push(month);
+				month = [];
+			}
+		}
+	);
 	
 	//legend extraction process
 	let legend = {};
 	let key = null;
-	 $('table[align="left"]').find('td').each(function(index, element){
-	 		if(index%2 === 0){
-	 			key = $(element).text().slice(0, -1);
-	 		}else{
-	 			if(key){
-	 				legend[key] = $(element).text();
-	 				key = null;
-	 			}
-	 		}
-	 	}
-	 );
+	$('table[align="left"]').find('td').each(function(index, element){
+			if(index%2 === 0){
+				key = $(element).text().slice(0, -1);
+			}else{
+				if(key){
+					legend[key] = $(element).text();
+					key = null;
+				}
+			}
+		}
+	);
 	return [schedule, legend];
 };
