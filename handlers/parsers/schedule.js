2adf787c72931e080d0f2fc8cce916d8ed667468
NihalSh <nihal.sh.1995@gmail.com>
2017-02-13 11:03:41 +0530

    schedule parser complete

--- handlers/parsers/schedule.js
+++ handlers/parsers/schedule.js
@@ -6,16 +6,37 @@ module.exports = function parser(body) {
 	
 	//header extraction
 	let schedule = [];
+	let header = [];
 	let month = [];
 	$('table[align="center"]').find('th').each(function(index, element){
 			if((index+1)%5 !== 0){
-				let content = {};
-				content[$(element).text()] = [];
-				month.push(content);
+				month.push($(element).text());
 			}else{
-				schedule.push(month);
+				header.push(month);
 				month = [];
 			}
+		}
+	);
+	
+	for(let i = 0; i < header.length; i++){
+		schedule.push([]);
+		for(let j = 0, content = {}; j < header[i].length; j++, content = {}){
+			content[header[i][j]] = [];
+			schedule[i].push(content);
+		}
+	}
+
+	$('table[align="center"]').find('tr:nth-of-type(n + 1)').each(function(outer, element){
+			$(element).find('td').each(function(inner, element){
+					if((inner+1)%5 !== 0){
+						if(((inner+1)%5 === 1) && !($(element).text())){
+							//date empty
+						}else{
+							schedule[ Math.floor(inner/5) ][ inner%5 ][ header [Math.floor(inner/5)] [inner%5] ].push($(element).text());
+						}
+					}
+				}
+			);
 		}
 	);
 	
