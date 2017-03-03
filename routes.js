0582ffdb004d7b03b6e3fa04a5f1be6005bde413
NihalSh <nihal.sh.1995@gmail.com>
2017-03-03 23:12:30 +0530

    HTTP codes added where appropriate

--- routes.js
+++ routes.js
@@ -9,10 +9,45 @@ let timetable = require('./handlers/timetable.js');
 
 module.exports = function(app){
 	app.get('/', login);
+	app.post('/', (req, res) =>
+		{
+			res.sendStatus(405);
+		}
+	);
 	app.get('/academic-schedule/:id', schedule);
+	app.post('/academic-schedule/:id', (req, res) =>
+		{
+			res.sendStatus(405);
+		}
+	);
 	app.get('/attendance/:id', attendance);
+	app.post('/attendance/:id', (req, res) =>
+		{
+			res.sendStatus(405);
+		}
+	);
 	app.get('/courses/:id', courses);
+	app.post('/courses/:id', (req, res) =>
+		{
+			res.sendStatus(405);
+		}
+	);
 	app.get('/details/:id', details);
+	app.post('/details/:id', (req, res) =>
+		{
+			res.sendStatus(405);
+		}
+	);
 	app.get('/logout/:id', logout);
+	app.post('/details/:id', (req, res) =>
+		{
+			res.sendStatus(405);
+		}
+	);
 	app.get('/timetable/:id', timetable);
+	app.post('/details/:id', (req, res) =>
+		{
+			res.sendStatus(405);
+		}
+	);
 };
