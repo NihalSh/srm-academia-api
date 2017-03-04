ece9ca971a7a3d9bc6ca349d00e95f1284270298
NihalSh <nihal.sh.1995@gmail.com>
2017-03-04 23:05:54 +0530

    uniform json response - details

--- handlers/parsers/details.js
+++ handlers/parsers/details.js
@@ -9,20 +9,22 @@ module.exports = function extractData(body) {
 	}
 	let key = "";
 	let regex = /([A-Za-z]+[\s]*[A-Za-z]+):/;
-	let details = {};
+	let head = [];
+	let content = [];
 	$('table[align="left"]').find('tr').each( function (index, element) {
 			$(element).find('td').each( function (index, element) {
 					if ($(element).find('strong').length) {
-						if (this.key.search(this.re) != -1) {
-							this.details[this.key.match(this.re)[1]] = $(element).text();
+						if (key.search(regex) != -1) {
+							head.push(key.match(regex)[1]);
+							content.push($(element).text());
 						}
-						this.key = "";
+						key = "";
 					} else {
-						this.key += $(element).text();
+						key += $(element).text();
 					}
-				}.bind({'key': key, 're': regex, 'details': details})
+				}
 			);
-		}.bind({'key': key, 're': regex, 'details': details})
+		}
 	);
-	return details;
+	return [head, content];
 };
