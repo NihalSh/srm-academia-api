2da6b50a07c847f8bc5810d6c2e4611bbb7f3bd8
NihalSh <nihal.sh.1995@gmail.com>
2017-03-10 10:29:32 +0530

    token shortened, response codes changed

--- handlers/login.js
+++ handlers/login.js
@@ -11,22 +11,42 @@ module.exports = function(req,res){
 		options['jar'] = j;
 		request(options, function(error, response, body){
 			if(!error){
-				let content = JSON.parse(body);
+				let content = null;
+				try {
+					content = JSON.parse(body);
+				} catch (exception) {
+					req.log.error(exception);
+					content = {};
+					content.error = 'invalid json';
+				}
 				if("error" in content){
 					req.log.info("login failed");
-					res.send('login failed');
+					res.sendStatus(400);
 				}else{
 					req.log.info("login successful");
-					res.send(querystring.escape(j.getCookieString(options.url)));
+					let token = null;
+					for (let cookie of j.getCookies(options.url)) {
+						if (cookie.hasOwnProperty('key')) {
+							if (cookie.key === "clientauthtoken") {
+								token = `${cookie.key}=${cookie.value}`;
+							}
+						}
+					}
+					if (token === null) {
+						res.sendStatus(400);
+					} else {
+						res.send(querystring.escape(token));
+					}
 				}
 			}else{
 					req.log.info("login request failed");
 					req.log.error(error);
+					res.sendStatus(400);
 			}
 		}
 	);
 	}else{
 		req.log.info("email and password undefined or empty");
-		res.send('login credentials not provided');
+		res.sendStatus(401);
 	}
 };
