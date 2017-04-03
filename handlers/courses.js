3c54df98c16381a4595e1469d2dd6773ed41de3a
NihalSh <nihal.sh.1995@gmail.com>
2017-04-03 18:13:38 +0530

    courses 2nd year (2015-2019)

--- handlers/courses.js
+++ handlers/courses.js
@@ -3,11 +3,13 @@ let request = require('request');
 const querystring = require('querystring');
 
 let options = require('./requests/course-confirmation-report.js');
+let optionsSecondYear = require('./requests/course-confirmation-report-second-year.js');
 let parser = require('./parsers/course-confirmation.js');
 
 module.exports = function(req, res){
 	if(req.params.id){
 		options.headers['Cookie'] = `clientauthtoken=${querystring.unescape(req.params.id)}`;
+		optionsSecondYear.headers['Cookie'] = `clientauthtoken=${querystring.unescape(req.params.id)}`;
 		request(options, function(error, response, body){
 				if(!error){
 					req.log.info("course request successful");
@@ -16,8 +18,24 @@ module.exports = function(req, res){
 						req.log.info("authorized user");
 						res.send(response);
 					}else{
-						req.log.info("unauthorized user");
-						res.sendStatus(400);
+						request(optionsSecondYear, function(error, response, body){
+								if(!error){
+									req.log.info("course request successful");
+									let response = parser(body);
+									if(response){
+										req.log.info("authorized user");
+										res.send(response);
+									}else{
+										req.log.info("unauthorized user");
+										res.sendStatus(400);
+									}
+								}else{
+									req.log.info("details request failed");
+									req.log.error(error);
+									res.send(504);
+								}
+							}
+						);
 					}
 				}else{
 					req.log.info("course request failed");
