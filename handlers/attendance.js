8d4be6f030feb6dc18dfe0ee11f92bfde1ff646e
NihalSh <nihal.sh.1995@gmail.com>
2017-04-10 22:41:07 +0530

    attendance code refactored

--- handlers/attendance.js
+++ handlers/attendance.js
@@ -1,33 +1,32 @@
-"use strict";
-let request = require('request');
-const querystring = require('querystring');
+"use strict"
+const request = require('request-promise-native')
+const querystring = require('querystring')
 
-let options = require('./requests/attendance.js');
-let parser = require('./parsers/attendance.js');
+let options = require('./requests/attendance.js')
+let parser = require('./parsers/attendance.js')
 
 module.exports = function(req, res){
 	if(req.params.id){
-		options.headers['Cookie'] = `clientauthtoken=${querystring.unescape(req.params.id)}`;
-		request(options, function(error, response, body){
-				if(!error){
-					req.log.info("attendance request successful");
-					let response = parser(body);
-					if(response){
-						req.log.info("authorized user");
-						res.send(response);
-					}else{
-						req.log.info("unauthorized user");
-						res.sendStatus(400);
-					}
+		options.headers['Cookie'] = `clientauthtoken=${querystring.unescape(req.params.id)}`
+		request(options)
+			.then( body => {
+				req.log.info("attendance request successful")
+				let response = parser(body)
+				if(response){
+					req.log.info("authorized user")
+					res.send(response)
 				}else{
-					req.log.info("attendance request failed");
-					req.log.error(error);
-					res.send(504);
-				}
-			}
-		);
+					req.log.info("unauthorized user")
+					res.sendStatus(400)
+				}				
+			})
+			.catch(err => {
+				req.log.info("attendance request failed")
+				req.log.error(error)
+				res.send(504)
+			})
 	}else{
-		req.log.info("token absent");
-		res.sendStatus(404);
+		req.log.info("token absent")
+		res.sendStatus(404)
 	}
-};
+}
