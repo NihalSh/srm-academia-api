77cb64cb2b8e8a82027d02c2477c2830740bdfa2
NihalSh <nihal.sh.1995@gmail.com>
2017-05-08 18:13:45 +0530

    added extra zeroes to maintain backwards compatibility with an app

--- handlers/attendance.js
+++ handlers/attendance.js
@@ -1,21 +1,28 @@
 "use strict"
-const request = require('request-promise-native')
 const querystring = require('querystring')
+
+const request = require('request-promise-native')
 
 let options = require('./requests/attendance.js')
 let parser = require('./parsers/attendance.js')
 
-module.exports = function(req, res){
-	if(req.params.id){
+module.exports = function (req, res) {
+	if (req.params.id) {
 		options.headers['Cookie'] = `clientauthtoken=${querystring.unescape(req.params.id)}`
 		request(options)
-			.then( body => {
+			.then(body => {
 				req.log.info("attendance request successful")
 				let response = parser(body)
-				if(response){
+				if (response) {
 					req.log.info("authorized user")
+					req.log.trace(response)
+					for (let i = 0; i < response.length; i++) {
+						if (response[i].length > 7) {
+							response[i].splice(6, 0, "0", "0")
+						}
+					}
 					res.send(response)
-				}else{
+				} else {
 					req.log.info("unauthorized user")
 					res.sendStatus(400)
 				}				
@@ -25,7 +32,7 @@ module.exports = function(req, res){
 				req.log.error(error)
 				res.send(504)
 			})
-	}else{
+	} else {
 		req.log.info("token absent")
 		res.sendStatus(404)
 	}
